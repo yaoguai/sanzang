e6f9c21105c131a4d2a71000f5325aecb7cb8dca
yaoguai <lapislazulitexts@gmail.com>
2014-09-22 09:21:57 -0500

    Refactoring and source code formatting / documentation.

--- setup.py
+++ setup.py
@@ -1,21 +1,29 @@
 #!/usr/bin/env python3
 
+""" Sanzang Utils setup script for packaging and installation. """
+
 from distutils.core import setup
 
 with open('README.rest', 'r', encoding='utf-8') as fin:
     LONG_DESCRIPTION = fin.read()
 
 setup(
-    name = 'sanzang-utils',
-    version = '1.0.4',
-    author = 'yaoguai',
-    author_email = 'lapislazulitexts@gmail.com',
-    url = 'https://github.com/yaoguai/sanzang-utils',
-    license = 'MIT',
-    description = 'Translate from Chinese, Japanese, or Korean.',
-    long_description = LONG_DESCRIPTION,
-    keywords = 'chinese japanese korean cjk asia language translation',
-    classifiers = [
+    #
+    # Basic information
+    #
+    name='sanzang-utils',
+    version='1.0.4',
+    author='yaoguai',
+    author_email='lapislazulitexts@gmail.com',
+    url='https://github.com/yaoguai/sanzang-utils',
+    license='MIT',
+    #
+    # Descriptions & classifiers
+    #
+    description='Translate from Chinese, Japanese, or Korean.',
+    long_description=LONG_DESCRIPTION,
+    keywords='chinese japanese korean cjk asia language translation',
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Developers',
@@ -27,22 +35,25 @@ setup(
         'Operating System :: POSIX',
         'Programming Language :: Python :: 3',
         'Topic :: Text Processing :: Linguistic',
-        'Topic :: Utilities' ],
+        'Topic :: Utilities'],
+    #
+    # Included Python files
+    #
     scripts=[
         'bin/szu-ed',
         'bin/szu-r',
         'bin/szu-ss',
-        'bin/szu-t' ],
+        'bin/szu-t'],
     data_files=[
-        ('share/doc/sanzang-utils', [
+        ['share/doc/sanzang-utils', [
             'AUTHORS.rest',
             'LICENSE.rest',
             'NEWS.rest',
             'README.rest',
-            'TUTORIAL.html' ]),
-        ('share/man/man1', [
+            'TUTORIAL.html']],
+        ['share/man/man1', [
             'man1/szu-ed.1',
             'man1/szu-r.1',
             'man1/szu-ss.1',
-            'man1/szu-t.1' ]) ]
+            'man1/szu-t.1']]]
 )
