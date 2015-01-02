71c695e1ff0d3915d4b3a02f560d7743196fbb8b
yaoguai <lapislazulitexts@gmail.com>
2015-01-03 00:38:34 +0800

    v1.1.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     # Basic information
     #
     name='sanzang-utils',
-    version='1.1.1',
+    version='1.1.2',
     author='yaoguai',
     author_email='lapislazulitexts@gmail.com',
     url='https://github.com/yaoguai/sanzang-utils',
